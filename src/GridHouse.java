606a86728e721848597ba0b1c140b0f95c8e2ae4
abhipaid <abhi.paid301@gmail.com>
2026-02-08 16:47:59 -0800

    Push #1

---
+++ src/GridHouse.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GridHouse extends JPanel {
+
+    public GridHouse() {
+        setVisible(true);
+        JLabel label = new JLabel("(Click 1 = START, Click 2 = END, Click 3 = toggle Obstacles");
+        add(label);
+    }
+}
