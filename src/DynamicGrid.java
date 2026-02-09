e1b668b4e28ea8ee13aa94f7bdf11b72853200e0
shannoncurtis <shannoncurtis40lb@gmail.com>
2026-02-08 19:41:04 -0800

    Adding enums and updating the constructor to be an empty grid upon init.

--- src/DynamicGrid.java
+++ src/DynamicGrid.java
@@ -8,13 +8,18 @@ import java.beans.PropertyChangeSupport;
 
 public class DynamicGrid extends JPanel implements PropertyChangeListener{
 
-    private JLabel cells[][];
+    // The grid represents the logical state of the array
+    // The labels are responsible for depicting what each cell represents
+    private CellType[][] grid;
+    private JLabel[][] labels;
+
     private int rows;
     private int cols;
     private boolean running = true;
     private boolean paused = false;
     private boolean finished = false;
 
+    // enum for the different types of cells
     enum CellType {
         EMPTY,
         START,
@@ -25,32 +30,33 @@ public class DynamicGrid extends JPanel implements PropertyChangeListener{
         PATH
     }
 
+    public DynamicGrid(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        grid = new CellType[rows][cols];
+        labels = new JLabel[rows][cols];
+        initGrid();
+    }
+
+    private void initGrid() {
+
+        // loop through each "cell" and assign it a label to later be used to show state of path finding algorithm
+        for (int r = 0; r < this.rows; r++) {
+            for (int c = 0; c < this.cols; c++) {
+                grid[r][c] = CellType.EMPTY;
+                JLabel label = new JLabel();
+                label.setOpaque(true);
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                label.setBackground(Color.WHITE);
+                labels[r][c] = label;
+                add(label);
+            }
+        }
+    }
 
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
     private void notifyChange() {pcs.firePropertyChange("grid", null, null);}
-
-    // Bind this callback function to input from JComboBox changes number of grids in GridHouse
-    public void setGridSize(int rows, int columns) {
-        removeAll();
-        setLayout( new GridLayout(rows, columns));
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                JLabel cell = new JLabel();
-                cell.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                cells[i][j] = cell;
-                cells[i][j].setVisible(true);
-                cells[i][j].setOpaque(true);
-                add(cell);
-            }
-        }
-        repaint();
-    }
-
-    public DynamicGrid(int rows, int cols) {
-        setGridSize(rows, cols);
-    }
 
 
     public void setRunning (boolean state) {
